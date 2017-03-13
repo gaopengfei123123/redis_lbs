3c95dda97488789aa799474050aa83713f8e4f11
gaofeifiy <5173180@qq.com>
2017-03-13 19:33:28 +0800

    调整下示例文件的内容

--- index.php
+++ index.php
@@ -2,45 +2,45 @@
 require_once __DIR__.'/vendor/autoload.php';
 
 
-$test = new \LBS\Services\LBSService();
+$test = new \LBS\Services\LBSServer();
 //$res = $test->searchByMembers('yabaolu',500,'km');
 
 //添加坐标
-//$add_params = [
-//    [
-//        'name' => 'yabao_road',
-//        'long' => '116.43620200729366',
-//        'lat' => '39.916880160714435'
-//    ],
-//    [
-//        'name' => 'jianguomen',
-//        'long' => '116.4356870231628',
-//        'lat' => '39.908560377800676'
-//    ],
-//    [
-//        'name' => 'chaoyangmen',
-//        'long' => '116.4345336732864',
-//        'lat' => '39.924466658329585'
-//    ],
-//    [
-//        'name' => 'galaxy_soho',
-//        'long' => '116.4335788068771',
-//        'lat' => '39.921372916981106'
-//    ],
-//    [
-//        'name' => 'cofco',
-//        'long' => '116.43564410781856',
-//        'lat' => '39.92024564137184'
-//    ],
-//    [
-//        'name' => 'fesco',
-//        'long' => '116.435182767868',
-//        'lat' => '39.91811857809279'
-//    ],
-//
-//
-//];
-//$res = $test->add($add_params);
+$add_params = [
+    [
+        'name' => 'yabao_road',
+        'long' => '116.43620200729366',
+        'lat' => '39.916880160714435'
+    ],
+    [
+        'name' => 'jianguomen',
+        'long' => '116.4356870231628',
+        'lat' => '39.908560377800676'
+    ],
+    [
+        'name' => 'chaoyangmen',
+        'long' => '116.4345336732864',
+        'lat' => '39.924466658329585'
+    ],
+    [
+        'name' => 'galaxy_soho',
+        'long' => '116.4335788068771',
+        'lat' => '39.921372916981106'
+    ],
+    [
+        'name' => 'cofco',
+        'long' => '116.43564410781856',
+        'lat' => '39.92024564137184'
+    ],
+    [
+        'name' => 'fesco',
+        'long' => '116.435182767868',
+        'lat' => '39.91811857809279'
+    ],
+
+
+];
+$res = $test->add($add_params);
 
 //$res2 = $test->del('gao1');
 
